729198eb5cfb57d4e394c5a0113133797acaacf3
Cristian <cristianrisueo@gmail.com>
2024-01-02 12:58:12 +0100

    feat_3: routing cleaning

--- app/(test)/other/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function OtherPage() {
-  return <div>Other Page</div>;
-}
--- app/(test)/something/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function SomethingPage() {
-  return <div>Excludes the test folder from the routing</div>;
-}
